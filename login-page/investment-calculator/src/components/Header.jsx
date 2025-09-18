7e8bb221016cd65df6a4502520339297c7f01b9e
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-09-18 18:46:50 -0300

    Dealing with some errors

--- login-page/investment-calculator/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return(
-        <header id="header">
-            <img src="investment-calculator-logo.png"/>
-            <h1>React Investment Calculator</h1>
-        </header>
-    )
-}
--- login-page/investment-calculator/src/components/InputArea.jsx
+++
@@ -1,8 +0,0 @@
-export default function InputArea({id, label, handleChange, value}){
-    return( 
-        <p key={id}> 
-            <label>{label}</label>
-            <input type="number" required value={value} onChange={handleChange}/>
-        </p>  
-    )
-}
