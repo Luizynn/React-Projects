df8e5b1e415a772072fe01f06e9ddbee3c27a37c
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-09-18 18:40:59 -0300

    Changing all global css with scoped styled components

--- login-page/src/components/AuthInputs.jsx
+++ login-page/src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { styled } from 'styled-components'
+import Button from './Button.jsx'
+import Input from './Input.jsx'
 
 const ControlContainer = styled.div`
   display: flex;
@@ -8,26 +10,16 @@ const ControlContainer = styled.div`
   margin-bottom: 1.5rem;
 `
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'} 
+
+const TextButton = styled.button`
+  color: #f0b322;
+  border: none;
+
+  &:hover{
+    color: #f0920e;
+  }
 `
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ?  '#fed2d2': '#d1d5db'};
-  color: ${({$invalid}) => ($invalid ? '#ef4444' : '#374151')};
-  border: 1px solid ${({$invalid}) => ($invalid ? '#f73f3f' : 'transparent')};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -51,30 +43,28 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid = {emailNotValid}>Email</Label>
           <Input
+            label="Email"
             type="email"
-            $invalid = {emailNotValid}
+            invalid = {emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid = {passwordNotValid}>Password</Label>
+        
           <Input
+            label = "Password"
             type="password"
-            $invalid = {passwordNotValid}
+            invalid = {passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
+        
       </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
+        <TextButton>
           Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        </TextButton>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
