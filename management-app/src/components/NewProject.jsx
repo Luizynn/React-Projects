30bedbe7e3837bceb9df95d6d924a0981e9a9bcf
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-10-08 10:59:26 -0300

    Working on NewProject component and lifting his state up

--- management-app/src/components/NewProject.jsx
+++ management-app/src/components/NewProject.jsx
@@ -1,47 +1,49 @@
 
-import { useState } from 'react'
-
 const labels = [
     {
         label: "title",
-        type: "text"
+        type: "text",
+        refKey: 'title',
+
     },
     {
         label: "description",
-        type: "text"
+        type: "text",
+        refKey: 'description',
+
     },
     {
         label: "due data",
-        type: "date"
+        type: "date",
+        refKey: 'date',
+        
     }
 
     ]
-export default function NewProject(){
+export default function NewProject({onHandleChange, onHandleSave}){
 
 
     return(
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-                <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+                <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={onHandleSave}>Save</button>
             </menu>
-            <p className="flex flex-col gap-1 my-4">
-                {labels.map((item) => {
-                    return(
-                        <>
-                            <label
-                                key={item.label} 
-                                className="text-sm font-bold uppercase text-stone-500"
-                                >
-                                    {item.label}
-                                </label>
-                            <input
-                                key={item.type} 
-                                className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" 
-                                type={item.type}/>
-                        </>)
-                    })}
-            </p>
+            {labels.map((item) => {
+                return(
+                    <p className="flex flex-col gap-1 my-4" key={item.refKey}>
+                        <label
+                            className="text-sm font-bold uppercase text-stone-500"
+                            >
+                                {item.label}
+                        </label>
+                        <input
+                            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" 
+                            type={item.type}
+                            onChange={(event) => onHandleChange(item.refKey, event.target.value)}
+                            value={item.refKey.current}/>
+                    </p>)
+                })}
         </div>
     )
 }
--- management-app/src/components/ProjectsSidebar.jsx
+++ management-app/src/components/ProjectsSidebar.jsx
@@ -1,19 +1,57 @@
 import DefaultScreen from "./DefaultScreen";
+import NewProject from "./NewProject";
+import { useState } from "react";
 
 export default function ProjectsSidebar(){
 
-    function handleClick(){
+    const [computedValues, setComputedValues] = useState({
+        title: undefined,
+        description: undefined,
+        date: undefined,
+    })
 
-    }
-    return(
+    const [projectState, setProjectState] = useState({
+        selectedScreen: 'default',
+        projects: [],
+    })
+
+    function handleChange(identifier, value){
+        setComputedValues((prevValues) => ({
+            ...prevValues,
+            [identifier]: value
+        }))
         
-        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-            <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
-            <div className="w-[35rem] mt-16">
-                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"> + Add project</button>
-            </div>
-            <ul className="mt-8">
-            </ul>
-        </aside>
+    }
+
+    function handleSave(){
+        projectState.projects.push(computedValues)
+        console.log("funciona", computedValues)
+        console.log(projectState.projects)
+    }
+
+    
+
+    function handleAddProject(){
+        setProjectState((prevState) => ({
+            ...prevState,
+            selectedScreen: 'adding'
+
+        }))
+    }
+
+    
+    return(
+        <>
+            <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+                <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
+                <div className="w-[35rem] mt-16">
+                    <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100" onClick={handleAddProject}> + Add project</button>
+                </div>
+                <ul className="mt-8">
+                </ul>
+            </aside>
+            {projectState.selectedScreen == 'default' && <DefaultScreen handleClick={handleAddProject}/>}
+            {projectState.selectedScreen == 'adding' && <NewProject onHandleChange={handleChange} onHandleSave={handleSave}/>}
+        </>
     )
 }
