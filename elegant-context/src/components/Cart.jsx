a6ffda6deb0e465156381177aefdf5eb63c32340
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-10-21 18:17:38 -0300

    Refactoring the code adding useContext to the application

--- elegant-context/src/components/Cart.jsx
+++ elegant-context/src/components/Cart.jsx
@@ -1,4 +1,10 @@
+import { use } from 'react';
+import { CartContext } from '../store/shopping-cart-context';
+
 export default function Cart({ items, onUpdateItemQuantity }) {
+
+const { items } = use(CartContext)
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
