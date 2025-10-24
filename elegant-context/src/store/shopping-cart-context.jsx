45347333b139d6ae141eaf2893d08f93673faaae
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-10-24 16:12:56 -0300

    Using the hook useReducer for the first time

--- elegant-context/src/store/shopping-cart-context.jsx
+++ elegant-context/src/store/shopping-cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useReducer } from 'react';
 import { DUMMY_PRODUCTS } from '../dummy-products';
 
 export const CartContext = createContext({
@@ -7,7 +7,18 @@ export const CartContext = createContext({
     updateItemQuantity: () => {},
 });
 
+function shoppingCartReducer(state, action){
+  return state;
+}
+
 export function AppCartContext({children}){
+
+    const [shoppingCartState, shoppingCartDispatch] = useReducer( 
+      shoppingCartReducer, 
+      {
+      items: [],
+      }
+  )
 
     const [shoppingCart, setShoppingCart] = useState({
         items: [],
@@ -70,7 +81,7 @@ export function AppCartContext({children}){
       }
     
       const ctxValue = {
-        items: shoppingCart.items,
+        items: shoppingCartState.items,
         addItemToCart: handleAddItemToCart,
         updateItemQuantity: handleUpdateCartItemQuantity,
       }
