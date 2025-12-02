5eafbe4b00aefce04e296cfac0a8fe366f477ed3
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-12-02 11:40:05 -0300

    Adding some new validations to forms

--- forms-app/src/components/Login.jsx
+++ forms-app/src/components/Login.jsx
@@ -6,8 +6,20 @@ export default function Login() {
     password:'',
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  })
+
+  const emailIsInvalid = didEdit.email !== '' && !enteredValues.email.includes('@')
+
   function handleSubmit(event) {
     event.preventDefault();
+
+    if(emailIsInvalid){
+      return;
+    }
+    
     console.log(enteredValues)
   }
 
@@ -15,6 +27,17 @@ export default function Login() {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value
+    }));
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: false
+    }))
+  }
+  
+  function handleInputBlur(identifier){
+    setDidEdit((prevValue) => ({
+      ...prevValue,
+      [identifier]: true
     }))
   }
 
@@ -29,10 +52,15 @@ export default function Login() {
             id="email" 
             type="email" 
             name="email"
-            onChange={(value) => handleEnteredValues('email', event.target.value)} 
+            onBlur={() => handleInputBlur('email')}
+            onChange={(event) => handleEnteredValues('email', event.target.value)} 
             value={enteredValues.email}
             />
+            <div className="control-error">
+              {emailIsInvalid && <p>Please enter a valid email adress.</p>}
+            </div>
         </div>
+
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
@@ -40,7 +68,7 @@ export default function Login() {
             id="password" 
             type="password" 
             name="password" 
-            onChange={(value) => handleEnteredValues('password', event.target.value)}
+            onChange={(event) => handleEnteredValues('password', event.target.value)}
             value={enteredValues.password}
             />
         </div>
