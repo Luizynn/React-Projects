6331a08f596a15ba263dee1955109dc444baba71
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2025-12-12 16:30:48 -0300

    Managing Form-dependent State with useActionState()

--- forms-actions/src/components/Signup.jsx
+++ forms-actions/src/components/Signup.jsx
@@ -1,8 +1,10 @@
+import { useActionState } from "react";
+
 import { isEmail, isEqualsToOtherValue, isNotEmpty, hasMinLength } from "../../../forms-app/src/util/validation";
 
 export default function Signup() {
 
-  function signupAction(formData){
+  function signupAction(prevFormState, formData){
     const email = formData.get("email")
     const password = formData.get("password")
     const confirmPassword = formData.get("confirm-password")
@@ -49,9 +51,10 @@ export default function Signup() {
     return { errors: null }
   }
 
+  const [formState, formAction] = useActionState(signupAction, { errors: null, })
 
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -135,6 +138,15 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && <ul className="error">
+        {formState.errors.map((error) => {
+        return(
+          <li key={error}>{error}</li>
+        )
+      })}
+      </ul>
+      }
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
