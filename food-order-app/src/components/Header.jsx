c2d2f3a3d2ca0b9fb07b2a445fc99dd426e79f17
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2026-01-19 16:19:08 -0300

    First header version

---
+++ food-order-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logoImg from '../assets/logo.png'
+
+export default function Header(){
+    return (
+        <header id='main-header'>
+            <div id='title'>
+                <img src={logoImg} alt="logo image" />
+                <h1>React food</h1>
+            </div>
+            <nav>
+                <button>Cart (0)</button>
+            </nav>
+        </header>
+    )
+}
