fc94c14eccf15a5cb5490f9482a666e896923e04
Luiz Eduardo <luiz.ecardoso@souunit.com.br>
2026-02-04 17:22:13 -0300

    feat: Creating the cart component to show the selected items and theirs name and price (for now)

---
+++ food-order-app/src/components/Cart.jsx
@@ -0,0 +1,39 @@
+import Modal from "../UI/Modal"
+import { useContext } from "react"
+import CartContext from "../store/CartContext"
+import { currencyFormatter } from "../util/formatting"
+import Button from "../UI/Button"
+import UserProgressCtx from "../store/UserProgressContext"
+
+export default function Cart(){
+    const cartCtx = useContext(CartContext)
+    const userProgressContext = useContext(UserProgressCtx)
+
+    function handleCloseCart () {
+        userProgressContext.closeCart()
+    }
+
+    const cartTotal = cartCtx.items.reduce((totalValue, currItem) => totalValue + currItem.quantity * currItem.price, 0)
+    
+    return(
+        <Modal className="cart" open={userProgressContext.progress === 'cart'}>
+            <h2>Your Cart</h2>
+            <ul>
+                {
+                    cartCtx.items?.map((item) => {
+                        return(
+                            <li key={item.id}>
+                                {item.name} - {item.quantity}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+            <p className="modal-actions">
+                <Button textOnly onClick={handleCloseCart}>Close</Button>
+                <Button>Go to checkout</Button>
+            </p>
+        </Modal>
+    )
+}
